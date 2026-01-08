b804992091404271071efe5407eb8df652007c01
Léo Cornillon <leo.cornillon@yupwego.com>
2026-01-08 18:34:38 +0100

    feat improve screen with refresh rate

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,11 @@
 package main
 
-import "github.com/lcor-io/chip8/internal/emulator"
+import (
+	"github.com/lcor-io/chip8/internal/emulator"
+)
 
 func main() {
 
-	emulator := emulator.Init()
-
-	for i := range 64 {
-		for j := range 32 {
-			emulator.Screen.SetPixel(i, j, ((i % (j + 1)) != 0))
-		}
-	}
-
-	emulator.Screen.Render()
+	emulator.Init()
 
 }
