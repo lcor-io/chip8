b9e277663ff5bcffa5b5a5c27af5472f66f63fd5
Léo Cornillon <leo.cornillon@yupwego.com>
2026-01-11 14:55:41 +0100

    fix some opcodes with the test rom

--- internal/emulator/opcode_8XY5.go
+++ internal/emulator/opcode_8XY5.go
@@ -5,6 +5,8 @@ func (e *emulator) opcode_8XY5(b1 uint8, b2 uint8) {
 
 	if e.Cpu.V[b1] > e.Cpu.V[b2] {
 		e.Cpu.V[0xF] = 1
+	} else {
+		e.Cpu.V[0xF] = 0
 	}
 
 	e.Cpu.V[b1] = e.Cpu.V[b1] - e.Cpu.V[b2]
