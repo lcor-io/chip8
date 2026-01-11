99ba68d61f33eb963fdeda042a8810710efe4eeb
Léo Cornillon <leo.cornillon@yupwego.com>
2026-01-11 15:11:12 +0100

    refacto move screen and cpu loop in different functions, so they don't interrupt each other

--- cmd/main.go
+++ cmd/main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 
 	emulator := emu.Init()
-	emulator.LoadROM("roms/tests.ch8")
+	emulator.LoadROM("roms/space_invaders.ch8")
 
 	// Start in TUI Mode
 	p := tea.NewProgram(emulator, tea.WithAltScreen(), tea.WithMouseCellMotion())
